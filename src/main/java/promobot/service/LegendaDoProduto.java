cfca49a761ac42e05201cbcb3a50e2ebfc6a46ca
Luis <luis.henriquenc10@gmail.com>
2026-07-20 17:48:55 -0300

    Formatação de produtos para função propria

---
+++ src/main/java/promobot/service/LegendaDoProduto.java
@@ -0,0 +1,25 @@
+package promobot.service;
+
+import promobot.model.Loja;
+import promobot.model.Produto;
+
+import java.util.Locale;
+
+public class LegendaDoProduto {
+
+    public String fotoProduto(Produto produto){
+        String fotoProduto = produto.getFoto();
+
+        return fotoProduto;
+    }
+
+    public String legendaFormatada(Produto produto){
+        String precoFormatado = String.format(Locale.of("pt", "BR"), "%.2f", produto.getPrecoPromocional()); //Adiciona os centavos com vírgula.
+        String legenda = "\uD83D\uDED2 #" + Loja.AMAZON + "\n\n" + "\uD83D\uDD25 " + produto.getNomeProduto() + "\n\n" + "\uD83D\uDCB0 Valor: R$" + precoFormatado + "\n" + produto.getLink();
+
+        return legenda;
+    }
+
+}
+
+
