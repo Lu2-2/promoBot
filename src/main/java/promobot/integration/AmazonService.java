1541495fac0cbde1bfc589a4d824a48056064a87
Luis <luis.henriquenc10@gmail.com>
2026-08-03 21:58:12 -0300

    Retrutura de rureqsição de API da Amazon parcialmente finalizada

--- src/main/java/promobot/integration/AmazonService.java
+++ src/main/java/promobot/integration/AmazonService.java
@@ -11,8 +11,8 @@ public class AmazonService {
     private HttpClient client = HttpClient.newHttpClient();
 
     public void autentificar(){
-        String clientId = "amzn1.application-oa2-client.4a4aad51050842a894a7e8465e80c524";
-        String clientSecret = "amzn1.oa2-cs.v1.01cb968dff66e0c7f4067d0fba997c14a6c30c08ae536fac2ab1d66ad69bddd5";
+        String clientId = "";
+        String clientSecret = "";
         String body = String.format("{\"grant_type\": \"client_credentials\", \"client_id\": \"%s\", \"client_secret\": \"%s\", \"scope\": \"creatorsapi::default\"}", clientId, clientSecret);
 
         HttpRequest request = HttpRequest.newBuilder()
