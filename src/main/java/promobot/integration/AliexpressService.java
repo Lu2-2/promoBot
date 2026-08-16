afe91ed4748187e1cc47ba8b088830a1b3e7ca4c
Luis <luis.henriquenc10@gmail.com>
2026-08-16 14:49:35 -0300

    Aliexpress: algoritmo de assinatura feito

--- src/main/java/promobot/integration/AliexpressService.java
+++ src/main/java/promobot/integration/AliexpressService.java
@@ -1,4 +1,78 @@
 package promobot.integration;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import javax.crypto.Mac;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.security.GeneralSecurityException;
+import java.util.Arrays;
+import java.util.Map;
+
+
+
 public class AliexpressService {
+
+    public static final String CHARSET_UTF8 = "UTF-8";
+    public static final String SIGN_METHOD_SHA256 = "sha256";
+    public static final String SIGN_METHOD_HMAC_SHA256 = "HmacSHA256";
+    private Dotenv dotenv = Dotenv.load();
+    private String appSecret = dotenv.get("ALIEXPRESS_APP_SECRET");
+
+    public String signApiRequest(Map<String, String> params, String signMethod, String apiName) throws IOException {
+        String[] keys = params.keySet().toArray(new String[0]);
+        Arrays.sort(keys);
+
+        StringBuilder query = new StringBuilder();
+        query.append(apiName);
+        for (String key : keys) {
+            String value = params.get(key);
+            if (areNotEmpty(key, value)) {
+                query.append(key).append(value);
+            }
+        }
+
+        byte[] bytes = null;
+
+        if (signMethod.equals(SIGN_METHOD_SHA256)) {
+            bytes = encryptHMACSHA256(query.toString(), appSecret);
+        }
+
+        return byte2hex(bytes);
+    }
+
+
+
+    private static byte[] encryptHMACSHA256(String data, String secret) throws IOException {
+        byte[] bytes = null;
+        try {
+            SecretKey secretKey = new SecretKeySpec(secret.getBytes(CHARSET_UTF8), SIGN_METHOD_HMAC_SHA256);
+            Mac mac = Mac.getInstance(secretKey.getAlgorithm());
+            mac.init(secretKey);
+            bytes = mac.doFinal(data.getBytes(CHARSET_UTF8));
+        } catch (GeneralSecurityException gse) {
+            throw new IOException(gse.toString());
+        }
+        return bytes;
+    }
+
+    public static String byte2hex(byte[] bytes) {
+        StringBuilder sign = new StringBuilder();
+        for (int i = 0; i < bytes.length; i++) {
+            String hex = Integer.toHexString(bytes[i] & 0xFF);
+            if (hex.length() == 1) {
+                sign.append("0");
+            }
+            sign.append(hex.toUpperCase());
+        }
+        return sign.toString();
+    }
+
+
+    private boolean areNotEmpty(String key, String value) {
+        return !key.isEmpty() && !value.isEmpty();
+    }
+
 }
+
